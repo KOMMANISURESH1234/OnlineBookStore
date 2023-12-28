e24e6d6b0317f03cfb1f601bb445ebdc85146b2d
KOMMANISURESH1234 <sathyasuresh358@gmail.com>
2023-12-28 11:42:26 +0530

    first commit

---
+++ onlineBookStore/src/app/book.ts
@@ -0,0 +1,8 @@
+export class Book {
+    public bookId!: number ;
+    public bookName!: string ; // Initialize with a default value
+    public bookAuthor!: string  ; // Initialize with a default value
+    public bookPrice!: number; // Initialize with a default value
+
+  }
+
